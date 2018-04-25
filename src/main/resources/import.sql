90b3d1b50821acfcdca5121af6f8f9a58f090488
Saber Hamidi <sh30g12@soton.ac.uk>
2018-04-25 17:01:05 +0100

    RESTful api code refactor

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO MOVIE(id, title, genre, rating) VALUES(1, 'The kite runner', 'Drama', '18');
+INSERT INTO MOVIE(id, title, genre, rating) VALUES(2, 'The horse runner', 'Romance', '22');
+INSERT INTO MOVIE(id, title, genre, rating) VALUES(3, 'The car runner', 'Drama', '18');
